cf698589c2b4af08a273eb3f914483d9ade0b862
Bhugol123 <bhugolkc07@gmail.com>
2024-07-13 19:36:47 -0700

    new branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import Header from './components/Header'
-import Display from './pages/home/Display'
-import Tech from './pages/home/Tech'
-import Intro from './pages/home/Intro'
-import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <Display/>
-      <Tech/>
-      <Intro/>
-      <Footer/>
-    </div>
+    <div>App</div>
   )
 }
 
